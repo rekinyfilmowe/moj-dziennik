dc267445088e1c97f4901be4cd092b10da36b591
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-08-10 00:18:07 +0200

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,18 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // po zalogowaniu leć na /app
+  return NextResponse.redirect(
+    new URL("/app", process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000")
+  );
+}
