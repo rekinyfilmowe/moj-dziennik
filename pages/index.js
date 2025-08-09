608dc43d0dc18fd3fa551c017e8646ef945901d1
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-08-09 21:35:47 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
-      <h1>Mój Dziennik</h1>
-      <p>Działa! 🚀</p>
-    </main>
-  );
-}
