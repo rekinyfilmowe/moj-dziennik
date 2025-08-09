dd10465364e635a4fc4bc21d81fac4055f5caadc
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-08-09 21:20:19 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-
+// pages/index.js
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Mój Dziennik</h1>
+      <p>Działa! 🚀</p>
+    </main>
+  );
+}
